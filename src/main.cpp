d59b7b53ce409caee10c5f82457f488cc891b7a5
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2015-03-17 18:34:41 +0100

    Add test of shape, reorganise test, improve gui management _ Use base class shape to perform shape drawing _ Reorganise tests by thematic with messages _ Improve gui management by creating functions to draw shape, clear gui etc

--- src/main.cpp
+++ src/main.cpp
@@ -19,27 +19,53 @@
 #include "polygon.hpp"
 #include "simple_gui.h"
 #include "convex_shape.hpp"
+#include "shape.hpp"
 #include <iostream>
 #include <vector>
 #include <math.h>
 #include <unistd.h>
 
+void draw_shape(simple_gui & p_gui,const shape & p_shape)
+{
+  uint32_t l_nb_segments = p_shape.get_nb_segment();
+  for(uint32_t l_index = 0 ; l_index < l_nb_segments ; ++l_index)
+    {
+      p_gui.draw_line(p_shape.get_segment(l_index).get_source().get_x()*30+10,
+		      p_shape.get_segment(l_index).get_source().get_y()*30+10,
+		      p_shape.get_segment(l_index).get_dest().get_x()*30+10,
+		      p_shape.get_segment(l_index).get_dest().get_y()*30+10,
+		      p_gui.getColorCode(255,0,0)
+		      );
+    }
+}
+
+void clear_gui(simple_gui & p_gui)
+{
+  p_gui.set_rectangle_without_lock(0,0,640,480,p_gui.getColorCode(0,0,0));
+  p_gui.refresh();
+}
+
 int main(void)
 {
+  std::cout << "------- TEST SEGMENT ---------" << std::endl ;
   point l_source(0,0);
   point l_dest(10,5);
   segment l_seg(l_source,l_dest);
 
   double l_x = 5;
   double l_y = l_seg.get_y(l_x);
+  std::cout << "Compute Y from X according to segment : " << std::endl ;
   std::cout << "(" << l_x << "," << l_y << ")" << std::endl ;
 
   l_y = 2.5;
   l_x = l_seg.get_x(l_y);
   std::cout << "(" << l_x << "," << l_y << ")" << std::endl ;
   
-  std::cout << "Belong : " << (l_seg.belong(point(l_x,l_y)) ? "oui" : "non") << std::endl ;
+  std::cout << "Compute X from Y according to segment : " << std::endl ;
+  std::cout << "Belong : " << (l_seg.belong(point(l_x,l_y)) ? "yes" : "no") << std::endl ;
 
+
+  std::cout << "--------- TEST VECTORIAL PRODUCT ------------" << std::endl ;
   point l_up(7,7);
   point l_down(5,0);
   point l_on(5,2.5);
@@ -48,6 +74,32 @@ int main(void)
   std::cout << "Vectorial product point Down : " << l_seg.get_side(l_down) << std::endl ;
   std::cout << "Vectorial product point On : " << l_seg.get_side(l_on) << std::endl ;
 
+
+  std::cout << "--------- TEST SCALAR PRODUCT ------------" << std::endl ;
+  point l_UN_ZERO(1,0);
+  point l_ZERO_UN(0,1);
+  point l_ZERO_ZERO(0,0);
+  point l_UN_UN(1,1);
+  segment l_seg1(l_ZERO_ZERO,l_UN_ZERO);
+  segment l_seg2(l_UN_ZERO,l_ZERO_UN);
+  double l_scalar = l_seg1.scalar_product(l_seg2);
+  std::cout << "Scalar product = " << l_scalar << std::endl ;
+
+  std::cout << "Square size seg1 : " << l_seg1.get_square_size() << std::endl ;
+  std::cout << "Square size seg2 : " << l_seg2.get_square_size() << std::endl ;
+
+  std::cout << "--------- TEST ANGLES ------------" << std::endl ;
+  double l_angle_rad = acos(l_scalar / (pow(l_seg1.get_square_size() * l_seg2.get_square_size(),0.5)));
+  std::cout << "Angle radian : " << l_angle_rad << std::endl ;
+  std::cout << "Angle degre : " << l_angle_rad * 180.0 / M_PI << std::endl ;
+  std::cout << (l_UN_ZERO < l_ZERO_UN) << std::endl ;
+  std::cout << (l_ZERO_UN < l_UN_ZERO) << std::endl ;
+  std::cout << (l_ZERO_ZERO < l_ZERO_ZERO) << std::endl ;
+
+  simple_gui l_gui;
+  l_gui.createWindow(640,480);
+
+  std::cout << "----------- DRAW TRAPEZ ----------" << std::endl ;
   point l_A(0,0);
   point l_B(5,0);
   point l_C(5,3);
@@ -62,7 +114,12 @@ int main(void)
   l_list.push_back(l_A);
 
   polygon l_polygon2(l_list);
+  draw_shape(l_gui,l_polygon2);
+  l_gui.refresh();
+  std::cout << "Polygon is convex " << ( l_polygon2.is_convex() ? "yes" : "no") << std::endl ;
+  sleep(10);
 
+  std::cout << "------- TEST CONCAV POLYGON ------------" << std::endl ;
   l_list.clear();
   l_list.push_back(point(0,0));
   l_list.push_back(point(5,0));
@@ -76,44 +133,23 @@ int main(void)
 
   std::cout << "Convex ? " << ( l_polygon.is_convex() ? "yes" : "no" ) << std::endl ;
 
-  point l_UN_ZERO(1,0);
-  point l_ZERO_UN(0,1);
-  point l_ZERO_ZERO(0,0);
-  point l_UN_UN(1,1);
-  segment l_seg1(l_ZERO_ZERO,l_UN_ZERO);
-  segment l_seg2(l_UN_ZERO,l_ZERO_UN);
-  double l_scalar = l_seg1.scalar_product(l_seg2);
-  std::cout << "Scalar product = " << l_scalar << std::endl ;
-
-  std::cout << "Square size seg1 : " << l_seg1.get_square_size() << std::endl ;
-  std::cout << "Square size seg2 : " << l_seg2.get_square_size() << std::endl ;
-
-  double l_angle_rad = acos(l_scalar / (pow(l_seg1.get_square_size() * l_seg2.get_square_size(),0.5)));
-  std::cout << "Angle radian : " << l_angle_rad << std::endl ;
-  std::cout << "Angle degre : " << l_angle_rad * 180.0 / M_PI << std::endl ;
-  std::cout << (l_UN_ZERO < l_ZERO_UN) << std::endl ;
-  std::cout << (l_ZERO_UN < l_UN_ZERO) << std::endl ;
-  std::cout << (l_ZERO_ZERO < l_ZERO_ZERO) << std::endl ;
-
-  simple_gui l_gui;
-  l_gui.createWindow(640,480);
-
-  uint32_t l_nb_segments = l_polygon.get_nb_segment();
-  for(uint32_t l_index = 0 ; l_index < l_nb_segments ; ++l_index)
-    {
-      l_gui.draw_line(l_polygon.get_segment(l_index).get_source().get_x()*30+10,
-		      l_polygon.get_segment(l_index).get_source().get_y()*30+10,
-		      l_polygon.get_segment(l_index).get_dest().get_x()*30+10,
-		      l_polygon.get_segment(l_index).get_dest().get_y()*30+10,
-		      l_gui.getColorCode(255,0,0)
-		      );
-    }
+  clear_gui(l_gui);
+  draw_shape(l_gui,l_polygon);
   l_gui.refresh();
+  sleep(10);
 
-  l_polygon.cut_in_convex_polygon();
-
-  convex_shape l_convex_shape(point(3,0),point(0,10),point(6,9));
+  clear_gui(l_gui);
+  
+  convex_shape l_convex_shape(point(1,0),point(10,0),point(10,10));
+  draw_shape(l_gui,l_convex_shape);
+  l_gui.refresh();
   std::cout << "Contains (0,0) ? " << l_convex_shape.contains(point(0,0)) << std::endl ;
-  std::cout << "Contains (3,5) ? " << l_convex_shape.contains(point(3,5)) << std::endl ;
+  std::cout << "Contains (3,1) ? " << l_convex_shape.contains(point(3,1)) << std::endl ;
+  std::cout << "Add (12,10) keep convexity : " << l_convex_shape.add(point(12,10)) << std::endl ;
+  sleep(10);
+  std::cout << "Add (1,10) keep convexity : " << l_convex_shape.add(point(1,10)) << std::endl ;
+  l_gui.set_rectangle_without_lock(0,0,640,480,l_gui.getColorCode(0,0,0));
+  draw_shape(l_gui,l_convex_shape);
+  l_gui.refresh();
   sleep(10);
 }
